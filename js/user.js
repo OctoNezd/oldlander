c55a749d1c2ca132e0376ca0ef1d65bb33a860b3
octo <me@octonezd.me>
2023-06-09 19:51:42 +0300

    Implement userscript version

---
+++ js/user.js
@@ -0,0 +1,10 @@
+import "./viewport.js";
+import "./general.js";
+import "./sidebar.js";
+import "./posts";
+import "~/css/hideAds.css";
+import "~/css/redditChanges.css";
+import "~/css/customUi.css";
+import "~/css/userTabIcons.css";
+import "~/css/resFullscreenGallery.css";
+console.log("oldlander user.js loaded");
