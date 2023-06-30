37b415fb23205149591a19c61e3744ce9ebf6771
octo <me@octonezd.me>
2023-06-30 20:06:01 +0300

    quiet down the posttoggles

--- js/posts/postToggles.js
+++ js/posts/postToggles.js
@@ -4,7 +4,6 @@ function setupGenericToggle(post, selector, onval) {
         console.error("Failed to find", selector, "skipping");
         return;
     }
-    console.log(button, post);
     let lastState = button.innerText.includes(onval);
     function setToggleClass() {
         if (lastState) {
