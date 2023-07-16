92240c20fb9c3183c0e4039f07ea6d06e895d4ca
faraplay <faraplay@outlook.com>
2023-07-16 18:10:33 +0100

    convert insertStyle.js to typescript

--- src/myStyleLoader/index.js
+++ src/myStyleLoader/index.js
@@ -1,15 +1,15 @@
-path = require("path");
+const path = require("path");
 
 module.exports = {
     pitch: function (request) {
         const insertStylePath = this.utils.contextify(
-            this.context, path.resolve(__dirname, "./insertStyle.js"));
+            this.context, path.resolve(__dirname, "./insertStyle.ts"));
         const cssPath = this.utils.contextify(this.context, `!!${request}`);
 
         const outputJs = `
         import insertStyle from ${JSON.stringify(insertStylePath)};
         import content from ${JSON.stringify(cssPath)};
-        insertStyle(content.toString());
+        insertStyle(content);
         `;
         return outputJs;
     },
