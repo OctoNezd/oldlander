7b3eb46b6f13307047ee52b57bc82520205a938a
faraplay <faraplay@outlook.com>
2023-07-16 16:44:45 +0100

    make utility functions generic

--- src/utility/querySelectorAsync.ts
+++ src/utility/querySelectorAsync.ts
@@ -1,7 +1,7 @@
 import { waitForElement } from "./waitForElement";
 
-export default function querySelectorAsync(selector: string) {
-    return new Promise<HTMLElement>((resolve, _reject) => {
-        waitForElement(selector, resolve);
+export default function querySelectorAsync<T extends HTMLElement>(selector: string) {
+    return new Promise<T>((resolve, _reject) => {
+        waitForElement<T>(selector, resolve);
     });
 }
