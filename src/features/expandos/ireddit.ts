0cf6eeba520619245ca03d6694ad618058c9f178
faraplay <faraplay@outlook.com>
2023-07-17 12:49:07 +0100

    convert features to typescript

--- src/features/expandos/ireddit.ts
+++ src/features/expandos/ireddit.ts
@@ -2,7 +2,7 @@ import BaseExpando from "./baseGallery";
 export default class iReddIt extends BaseExpando {
     sitename = "i.redd.it";
     urlregex = new RegExp(/https:\/\/i\.redd\.it\/.{13}.{3,}/);
-    async createGalleryData(post) {
+    async createGalleryData(post: HTMLDivElement) {
         return new Map([[post.dataset.url, ""]]);
     }
 }
