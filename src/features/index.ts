2ca6ed43d55b452e916e01669a34b7c6a97ab03d
Yan <me@octonezd.me>
2023-12-30 20:24:01 +0300

    Implement toggle for marquee, fix #79

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,5 +1,6 @@
 import ConsoleSave from "./console_dump";
 import Expandos from "./expandos";
+import Marquee from "./marquee";
 import RESButtons from "./resButtons";
 import Sidebar from "./sidebar";
-export default [ConsoleSave, Expandos, Sidebar, RESButtons];
+export default [ConsoleSave, Expandos, Sidebar, RESButtons, Marquee];
---
+++ src/features/marquee.ts
@@ -0,0 +1,20 @@
+import querySelectorAsync from "../utility/querySelectorAsync";
+import { waitForElement } from "../utility/waitForElement";
+import { OLFeature, SettingToggle } from "./base";
+
+const marqueeSettingId = "marquee";
+
+export default class Marquee extends OLFeature {
+    moduleId = "marquee";
+    moduleName = "Subreddit marquee list";
+    async init() {
+        this.settingOptions.push(
+            new SettingToggle("Marquee toggle", "Enables old reddit marquee", marqueeSettingId, async (toggle) => {
+                document.documentElement.classList.toggle("redditMarqueEnabled", toggle)
+            })
+        );
+        waitForElement("#sr-header-area", (element) => {
+            document.body.appendChild(element)
+        })
+    }
+}
