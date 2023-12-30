9f37daf0e6377b8495035b67b6dc85fd552caee2
Yan <me@octonezd.me>
2023-12-30 19:27:02 +0300

    Reload page if oldlander already loaded

--- src/user.ts
+++ src/user.ts
@@ -4,5 +4,9 @@ import "./styling";
 import "./header";
 import "./posts";
 import "./redditPreferences";
-
 console.log("oldlander userscript loaded");
+if (document.documentElement.classList.contains("oldlander")) {
+    location.reload()
+} else {
+    document.documentElement.classList.add("oldlander");
+}
