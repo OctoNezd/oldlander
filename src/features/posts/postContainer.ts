2a1b9ec0c597cc767e7084716d005690cbdd558c
Yan <me@octonezd.me>
2024-01-03 15:15:09 +0300

    Reimplement votes, fix #86

--- src/features/posts/postContainer.ts
+++ src/features/posts/postContainer.ts
@@ -1,15 +1,10 @@
+import "./css/postContainer.css"
 export default function setupPostContainer(post: HTMLDivElement) {
     // create new postContainer div to hold post & expando preview
     const postContainer = document.createElement("div");
     postContainer.append(...post.children)
     Object.assign(postContainer.dataset, post.dataset)
     postContainer.classList.add("ol-post-container");
-    // const clearLeft = post.nextElementSibling; // i don't know what this div is but let's put it inside too
-    // post.before(postContainer);
-    // postContainer.appendChild(post);
-    // if (clearLeft) {
-        // postContainer.appendChild(clearLeft);
-    // }
     post.appendChild(postContainer);
     const buttons = (post.querySelector(".flat-list.buttons") as HTMLDivElement)
     const entry = post.querySelector(".entry")
