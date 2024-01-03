207b67809389500f9ebdc3bb7e0eb756b045ca04
Yan <me@octonezd.me>
2024-01-03 15:31:01 +0300

    Implement dark/white theme controls. Fix #56

--- src/features/index.ts
+++ src/features/index.ts
@@ -5,7 +5,8 @@ import Sidebar from "./sidebar";
 import RESCompatibility from "./RESCompatibility";
 import PostsEnhancements from "./posts";
 import { OLFeature } from "./base";
+import WhiteTheme from "./themeSwitch";
 type Constructor = new (...args: any[]) => OLFeature;
-const features: Array<Constructor> = [Expandos, RESButtons, Sidebar,  RESCompatibility, RedditMarquee];
+const features: Array<Constructor> = [Expandos, RESButtons, Sidebar,  RESCompatibility, RedditMarquee, WhiteTheme];
 features.push(...PostsEnhancements)
 export default features;
