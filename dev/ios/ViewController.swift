adb197a54df8b239376fab58c3873d070e155dd2
OctoNezd <me@octonezd.me>
2024-03-20 20:54:23 +0300

    try custom viewcontroller to prevent ios stupidity

---
+++ dev/ios/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  OldLander
+//
+//  Created by runner on 1/3/24.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
+
+    @IBOutlet var webView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.webView.navigationDelegate = self
+        self.webView.scrollView.isScrollEnabled = false
+
+        self.webView.configuration.userContentController.add(self, name: "controller")
+
+        self.webView.loadFileURL(Bundle.main.url(forResource: "Main", withExtension: "html")!, allowingReadAccessTo: Bundle.main.resourceURL!)
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        // Override point for customization.
+    }
+
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        // Override point for customization.
+    }
+
+    // https://stackoverflow.com/a/64909961
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        guard case .linkActivated = navigationAction.navigationType,
+              let url = navigationAction.request.url
+        else {
+            decisionHandler(.allow)
+            return
+        }
+        decisionHandler(.cancel)
+        UIApplication.shared.openURL(url)
+   }
+
+}
